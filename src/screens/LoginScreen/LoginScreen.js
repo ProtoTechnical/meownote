afcba14444e074c9a8e3843d615c47fa8ca308db
Prototechnical <prototechnical@github.com>
2021-09-11 21:00:01 -0700

    Test push from visual studio

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -11,7 +11,7 @@ export default function LoginScreen({ navigation }) {
    const onFooterLinkPress = () => {
       navigation.navigate('Registration')
    }
-
+    //Test comment
    const onLoginPress = () => {
       firebase
          .auth()
